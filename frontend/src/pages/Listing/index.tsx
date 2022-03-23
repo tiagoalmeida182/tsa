2bed51d65ad2b1c73b92a8bc42d2b8f3b1783256
Tiago Almeida <sptiagoalmeida@gmail.com>
2022-03-23 19:45:59 -0300

    Subindo Layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -21,13 +21,14 @@ function Listing() {
         empty: true
     });
 
-
     useEffect(() => {
         axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`).then(response => {
             const data = response.data as MoviePage;
             setPage(data);
+
         });
-    }, []);
+    }, [pageNumber]);
+
 
     return (
 
